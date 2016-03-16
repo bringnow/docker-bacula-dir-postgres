7c39246266a58c426b9f683b9db05dc6ec6c20a4
Fabian Köster <koesterreich@fastmail.fm>
2016-03-16 08:00:48 +0100

    Fix first run script, dpass password via env variable

--- first_run.sh
+++ first_run.sh
@@ -4,6 +4,9 @@ cd /usr/libexec/bacula/
 
 PSQL_OPTS="-h ${DB_PORT_5432_TCP_ADDR} -U postgres"
 
-./create_postgresql_database ${PSQL_OPTS}
-./make_postgresql_tables ${PSQL_OPTS}
-./grant_postgresql_privileges ${PSQL_OPTS}
+export PGPASSWORD=${DB_ENV_POSTGRES_PASSWORD}
+
+createuser ${PSQL_OPTS} -d -R bacula
+./create_bacula_database ${PSQL_OPTS}
+./make_bacula_tables  ${PSQL_OPTS}
+./grant_bacula_privileges ${PSQL_OPTS}
