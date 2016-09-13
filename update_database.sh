b2c6ee7279c4cb4cf9cb905f6b56995846d16dc1
Fabian Köster <mail@fabian-koester.com>
2016-09-13 10:25:35 +0200

    Add script for updating database

---
+++ update_database.sh
@@ -0,0 +1,19 @@
+#! /bin/bash
+
+die() {
+    echo >&2 "[`date +'%Y-%m-%d %T'`] $@"
+    exit 1
+}
+
+log() {
+  if [[ "$@" ]]; then echo "[`date +'%Y-%m-%d %T'`] $@";
+  else echo; fi
+}
+
+cd /usr/libexec/bacula/
+
+PSQL_OPTS="-h ${DB_HOST} -U postgres"
+
+./update_postgresql_tables ${PSQL_OPTS} || die "Failed to update database"
+
+log "Database update completed without errors"
