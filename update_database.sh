71ac83d66f9c3d84ede1d8c1c165eb16e2a7e664
Fabian Köster <mail@fabian-koester.com>
2016-09-16 07:56:45 +0200

    Run "grant_bacula_privileges" after db upgrade

--- update_database.sh
+++ update_database.sh
@@ -15,5 +15,6 @@ cd /usr/libexec/bacula/
 PSQL_OPTS="-h ${DB_HOST} -U postgres"
 
 ./update_postgresql_tables ${PSQL_OPTS} || die "Failed to update database"
+./grant_bacula_privileges ${PSQL_OPTS} || die "Failed to grant bacula priviledges"
 
 log "Database update completed without errors"
