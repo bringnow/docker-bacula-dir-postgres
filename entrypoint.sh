6094cc026bf2fe2702dd348b3bdca33fab031e3e
Fabian Köster <mail@fabian-koester.com>
2016-11-03 17:39:45 +0100

    Fix nullmailer not starting issue

--- entrypoint.sh
+++ entrypoint.sh
@@ -16,7 +16,7 @@ BACULA_DIR_COMMAND="/usr/sbin/bacula-dir -c ${BACULA_DIR_CONFIG}"
 
 /usr/local/bin/create_dhparam.sh || die "Failed to generate dhparam"
 
-/usr/sbin/nullmailer-send -d || die "Failed to start nullmailer daemon"
+/etc/init.d/nullmailer start || die "Failed to start nullmailer daemon"
 
 rm -fv ${BACULA_DIR_PID_FILE} || die "Failed to remove stale PID file"
 
